dc469db57b2a3328941575e975eb168e9fc11080
Cedric Matthew Verdida <70793428+azumic@users.noreply.github.com>
2021-03-21 02:43:51 +0800

    Initial Commit

---
+++ gridsome.config.js
@@ -0,0 +1,13 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Cedric Verdida',
+  message: 'Nice to meet ya!',
+  titleTemplate: 'Welcome to my world HAHA HAHA',
+  siteDescription: 'Hello I am Cedric Matthew Verdida this is my portfolio!',
+  plugins: []
+}
